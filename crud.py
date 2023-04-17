4739cea01f1e80c61d4e0e4959314707b7036bb4
Angelika Mazurek <angelika.a.maurek@gmail.com>
2023-04-17 15:31:35 -0700

    create and finish model.py and crud.py

---
+++ crud.py
@@ -0,0 +1,73 @@
+from model import db, User, Recipe, Rating, Ingredient, connect_to_db
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    return user
+
+
+def get_users():
+    """Return all users."""
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+
+    return User.query.get(user_id)
+
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+
+def create_recipe(title, description):
+    """Create and return a new recipe."""
+
+    recipe = Recipe (
+        title=title,
+        description=description,
+        )
+    
+    return recipe
+
+
+def get_recipes():
+    """Return all recipes"""
+
+    return Recipe.query.all()
+
+
+def get_recipe_by_id(recipe_id):
+    """Return a recipe by primary key"""
+    
+    return Recipe.query.get(recipe_id)
+
+
+def create_ingredients(name, quantiti, unit):
+    """Create and return new ingredient"""
+    
+    ingredient = Ingredient(name=name, quantiti=quantiti, unit=unit)
+
+    return ingredient
+
+
+
+def create_rating(user, recipe, score):
+    """Create and return a new rating."""
+
+    rating = Rating(user=user, recipe=recipe, score=score)
+
+    return rating
+
+
+def update_rating(rating_id, new_score):
+    """ Update a rating given rating_id and the updated score. """
+    
+    rating = Rating.query.get(rating_id)
+    rating.score = new_score
+
---
+++ model.py
@@ -0,0 +1,102 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """Data model for a users."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"<User user_id={self.user_id} email={self.email}>"
+    
+    recipes = db.relationship("Recipe", back_populates="author")
+    favorites = db.relationship("Favorite", back_populates="user")
+    ratings = db.relationship("Rating", back_populates="user")
+
+
+
+class Recipe(db.Model):
+
+    __tablename__ = "recipes"
+
+    recipe_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"<Recipe recipe_id={self.recipe_id} title={self.title}>"
+    
+    author = db.relationship("User", back_populates="recipes")
+    favorites = db.relationship("Favorite", back_populates="recipe")
+    ratings = db.relationship("Rating", back_populates="recipe")
+    ingredients = db.relationship("Ingredient", back_populates="recipe")
+
+class Rating(db.Model):
+
+    __tabelename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.recipe_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Rating rating_id={self.rating_id} score={self.score}>"
+    
+    recipe = db.relationship("Recipe", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
+    
+
+    
+class Favorite(db.Model):
+
+    __tablename__ = "favorites"
+
+    favorite_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.recipe_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Favorite favorite_id={self.favorite_id} recipe_id={self.recipe_id}>"
+    
+    recipe = db.relationship("Recipe", back_populates="favorites")
+    user = db.relationship("User", back_populates="favorites")
+
+class Ingredient(db.Model):
+
+    __tablename__ = "ingredients"
+
+    ingredient_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.recipe_id"), nullable=False)
+    name = db.Column(db.String, nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    unit = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"<Ingredient ingredient_id={self.ingredient_id} name{self.name} quantity{self.quantity} unit{self.unit}>"
+    
+    recipe = db.relationship("Recipe", back_populates="ingredients")
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///kitchen_helper", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+if __name__ == "__main__":
+    from flask import Flask
+
+    app = Flask(__name__)
+    connect_to_db(app)
